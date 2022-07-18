28b495df994e09a3d195e80062e8a94a1ac59d62
Taru Ghiya <tarughiya@gmail.com>
2022-07-18 13:04:27 +0530

    Fixed indentation

--- app/src/main/java/com/example/closedprs/Model/Post.kt
+++ app/src/main/java/com/example/closedprs/Model/Post.kt
@@ -2,12 +2,11 @@ package com.example.closedprs.Model
 
 import com.google.gson.annotations.SerializedName
 
-
 data class Post(
- val title: String?,
- @SerializedName("created_at")
- val created_date: String?,
- @SerializedName("closed_at")
- val closed_date: String?,
- val user: User
+    val title: String?,
+    @SerializedName("created_at")
+    val created_date: String?,
+    @SerializedName("closed_at")
+    val closed_date: String?,
+    val user: User
 )
